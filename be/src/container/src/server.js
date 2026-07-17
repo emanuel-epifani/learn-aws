331b9268f9c202a2ef6becd421c6b889cc5dd745
emanuelepifani <emanuel.epifani.m@gmail.com>
2026-07-18 00:04:49 +0200

    fix: add CORS headers to ECS backend

--- be/src/container/src/server.js
+++ be/src/container/src/server.js
@@ -3,6 +3,19 @@ const { Pool } = require("pg");
 const { CognitoJwtVerifier } = require("aws-jwt-verify");
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
